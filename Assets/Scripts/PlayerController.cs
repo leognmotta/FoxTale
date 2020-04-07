aa4825c71120480bb68306f59f7aa97972bdb4c1
Leonardo Motta <mail@leomotta.me>
2020-04-07 03:46:30 -0300

    feat: create first animation, and Player double jump

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,16 +1,41 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Serialization;
 
 public class PlayerController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-    }
+    [SerializeField] private float moveSpeed;
+    [SerializeField] private float jumpForce;
+    [SerializeField] private Rigidbody2D playerRb;
+    [SerializeField] private Transform groundCheck;
+    [SerializeField] private LayerMask whatIsGround;
 
-    // Update is called once per frame
-    void Update()
+    private const float GroundedRadius = .2f;
+    private bool _canDoubleJump;
+    private bool _isGrounded;
+
+
+    private void Update()
     {
+        playerRb.velocity = new Vector2(moveSpeed * Input.GetAxisRaw("Horizontal"), playerRb.velocity.y);
+
+        _isGrounded = Physics2D.OverlapCircle(groundCheck.position, GroundedRadius, whatIsGround);
+
+        if (_isGrounded)
+            _canDoubleJump = true;
+
+        if (Input.GetButtonDown("Jump"))
+        {
+            if (_isGrounded)
+            {
+                playerRb.velocity = new Vector2(playerRb.velocity.x, jumpForce);
+            }
+            else if (_canDoubleJump)
+            {
+                playerRb.velocity = new Vector2(playerRb.velocity.x, jumpForce);
+                _canDoubleJump = false;
+            }
+        }
     }
 }
