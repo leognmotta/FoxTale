158d7460fc89be34c262141c47b2d29506da788c
Leonardo Motta <mail@leomotta.me>
2020-04-08 00:35:03 -0300

    feat: add camera move and background parallax

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    [SerializeField]
+    private Transform target;
+    [SerializeField]
+    private Transform farBackground;
+    [SerializeField]
+    private Transform middleBackground;
+    [SerializeField]
+    private float minHeight;
+    [SerializeField]
+    private float maxHeight;
+
+    private const float CameraZIndex = -10f;
+
+    private Vector2 _lastPosition;
+    private Vector2 _amountToMove;
+    private float _clampedY;
+
+    private void Awake()
+    {
+        _lastPosition = transform.position;
+    }
+
+    private void FixedUpdate()
+    {
+        var targetPosition = target.position;
+        var cameraPosition = transform.position;
+        _amountToMove = new Vector2(cameraPosition.x - _lastPosition.x, cameraPosition.y - _lastPosition.y);
+        _clampedY = Mathf.Clamp(targetPosition.y, minHeight, maxHeight);
+
+        transform.position = new Vector3(targetPosition.x, _clampedY, CameraZIndex);
+
+        farBackground.position += new Vector3(_amountToMove.x, _amountToMove.y * .2f, 0f);
+        middleBackground.position += new Vector3(_amountToMove.x, 0f, 0f) * .5f;
+
+        _lastPosition = cameraPosition;
+    }
+}
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,41 +1,110 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Serialization;
 
 public class PlayerController : MonoBehaviour
 {
-    [SerializeField] private float moveSpeed;
-    [SerializeField] private float jumpForce;
-    [SerializeField] private Rigidbody2D playerRb;
-    [SerializeField] private Transform groundCheck;
-    [SerializeField] private LayerMask whatIsGround;
+    private static readonly int IsRunning = Animator.StringToHash("isRunning");
+    private static readonly int IsGrounded = Animator.StringToHash("isGrounded");
+    private static readonly int YVelocity = Animator.StringToHash("yVelocity");
+
+    [Space]
+    [Header("Movement Stats")]
+    [SerializeField]
+    private float moveSpeed;
+    [SerializeField]
+    private float jumpForce;
+
+    [Space]
+    [Header("Ground Definition")]
+    [SerializeField]
+    private Transform groundCheck;
+    [SerializeField]
+    private LayerMask whatIsGround;
 
     private const float GroundedRadius = .2f;
+
+    private Rigidbody2D _playerRb;
+    private Animator _animator;
+    private SpriteRenderer _spriteRenderer;
+    private float _horizontalMove;
     private bool _canDoubleJump;
     private bool _isGrounded;
+    private bool _jump;
+    private bool _isRunning;
 
+    private void Awake()
+    {
+        _playerRb = GetComponent<Rigidbody2D>();
+        _animator = GetComponent<Animator>();
+        _spriteRenderer = GetComponent<SpriteRenderer>();
+    }
 
     private void Update()
     {
-        playerRb.velocity = new Vector2(moveSpeed * Input.GetAxisRaw("Horizontal"), playerRb.velocity.y);
+        GetInputs();
+        UpdateAnimations();
+        Flip();
+    }
 
-        _isGrounded = Physics2D.OverlapCircle(groundCheck.position, GroundedRadius, whatIsGround);
+    private void FixedUpdate()
+    {
+        Move();
+    }
+
+    private void UpdateAnimations()
+    {
+        _animator.SetBool(IsRunning, _horizontalMove > 0 || _horizontalMove < 0);
+        _animator.SetBool(IsGrounded, _isGrounded);
+        _animator.SetFloat(YVelocity, _playerRb.velocity.y);
+    }
+
+    private void Flip()
+    {
+        if (_horizontalMove < 0)
+            _spriteRenderer.flipX = true;
+        else if (_horizontalMove > 0)
+            _spriteRenderer.flipX = false;
+    }
+
+    private void GetInputs()
+    {
+        _horizontalMove = Input.GetAxisRaw("Horizontal") * moveSpeed;
 
         if (_isGrounded)
             _canDoubleJump = true;
 
         if (Input.GetButtonDown("Jump"))
+            _jump = true;
+    }
+
+    private void Move()
+    {
+        _isGrounded = Physics2D.OverlapCircle(groundCheck.position, GroundedRadius, whatIsGround);
+
+        _playerRb.velocity = new Vector2((_horizontalMove * Time.fixedDeltaTime) * 10f, _playerRb.velocity.y);
+
+
+        if (_jump && !_isGrounded && !_canDoubleJump)
         {
-            if (_isGrounded)
-            {
-                playerRb.velocity = new Vector2(playerRb.velocity.x, jumpForce);
-            }
-            else if (_canDoubleJump)
-            {
-                playerRb.velocity = new Vector2(playerRb.velocity.x, jumpForce);
-                _canDoubleJump = false;
-            }
+            _jump = false;
+        }
+
+        if (!_jump) return;
+
+        if (_isGrounded)
+        {
+            _playerRb.velocity = new Vector2(_playerRb.velocity.x, (jumpForce * Time.fixedDeltaTime) * 10f);
+            _jump = false;
+        }
+        else if (_canDoubleJump && !_isGrounded)
+        {
+            _playerRb.velocity =
+                new Vector2(_playerRb.velocity.x, ((jumpForce * Time.fixedDeltaTime) * 10f) / 1.2f);
+            _canDoubleJump = false;
+            _jump = false;
         }
     }
 }
